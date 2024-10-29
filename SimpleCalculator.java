c1b2f30492b304079f6d16ee86e521fd0b2bccb2
jonesisaacJOE <isaacongudu@gmail.com>
2024-10-29 16:54:53 -0700

    initial commit with project files

---
+++ SimpleCalculator.java
@@ -0,0 +1,16 @@
+public class SimpleCalculator{
+
+    public static void multiplynumber(){
+        int num1 = 4;
+        int num2 = 10;
+        int answer = num1*num2;
+
+         System.out.println(answer);
+
+    }
+    public static void main(String[]args){
+        SimpleCalculator wee = new SimpleCalculator();
+         wee.multiplynumber();     
+    }
+
+}
